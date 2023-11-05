88ca1f539ea22fdad7b7712a0c17e8501447fad5
SriHarsha <jalesriharshareddy26@gmail.com>
2023-11-04 23:49:06 -0500

    Creating Dashboard for each usertype

---
+++ admin/welcome.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def welcome():
+    """Displays a welcome message to the user."""
+    st.write("Welcome to the welcome page!")
+
+if __name__ == "__main__":
+    welcome()
