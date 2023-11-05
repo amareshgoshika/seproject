e596a663994c7e6ce61df92f6071142555941a3f
Amaresh <amargoshika@gmail.com>
2023-11-05 01:16:16 -0600

    Creating Project

---
+++ admin/pages/viewStudent.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+def viewStudent():
+    # Sample data as a list of dictionaries
+
+    # Sample data as a list of dictionaries
+    data = [
+        {"Name": "Alice", "Age": 25, "City": "New York"},
+        {"Name": "Bob", "Age": 30, "City": "Los Angeles"},
+        {"Name": "Charlie", "Age": 22, "City": "Chicago"},
+        {"Name": "David", "Age": 28, "City": "San Francisco"},
+    ]
+
+    # Display the list data as a table
+    st.table(data)
+
